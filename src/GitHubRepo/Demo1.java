9039498dc09992fe0386b6fc807b293affccdc37
ThenrajM <thenraj2743555@gmail.com>
2026-03-25 11:13:23 +0530

    GitHib

---
+++ src/GitHubRepo/Demo1.java
@@ -0,0 +1,17 @@
+package GitHubRepo;
+
+public class Demo1 {
+	 public static void main(String[] args) {
+		  int a =15;
+		  int b = 25;
+		  int add = a + b;
+	        int sub = a - b;
+	        int mul = a * b;
+	        int div = a / b;
+	     
+	        System.out.println("Addition: " + add);
+	        System.out.println("Subtraction: " + sub);
+	        System.out.println("Multiplication: " + mul);
+	        System.out.println("Division: " + div);
+}
+	 }
