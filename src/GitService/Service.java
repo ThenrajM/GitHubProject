f65e64faac9c237192e46607c4417e059b1e430b
ThenrajM <thenraj2743555@gmail.com>
2026-03-25 12:53:17 +0530

    class created

---
+++ src/GitService/Service.java
@@ -0,0 +1,15 @@
+package GitService;
+
+public class Service {
+	 public static void main(String[] args) {
+		  int a =15;
+		  int b = 25;
+		  int add = a + b;
+	        int sub = a - b;
+	        int mul = a * b;
+	        int div = a / b;
+	     
+	        System.out.println("Addition: " + add);
+	        System.out.println("Subtraction: " + sub);
+}
+}
